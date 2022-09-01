699d6c270e040f814631ada1e939fef32cca7653
Elysena <eliana.cardenas17@misena.edu.co>
2022-09-01 08:28:09 -0500

    tabla materias

---
+++ database/migrations/2022_08_25_085533_create_materias_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->id('materia');
+            $table->timestamps();
+            $table->string('nombreMateria');
+            $table->integer('intensidadHoraria');
+            $table->integer('id_Estudiante');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materias');
+    }
+};
